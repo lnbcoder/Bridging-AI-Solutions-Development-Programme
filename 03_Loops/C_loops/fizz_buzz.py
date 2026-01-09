b1981995b2315eb68387246ff196e2ad03991037
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-01-09 12:28:42 +0000

    updated

--- 03_Loops/C_loops/fizz_buzz.py
+++ 03_Loops/C_loops/fizz_buzz.py
@@ -22,7 +22,7 @@
 
 def fizz_buzz(max_num):
     for i in range(1, max_num + 1):
-        if i % 3 == 0 or i % 5 == 0:
+        if (i % 3 == 0) ^ (i % 5 == 0):
             print(i)
 
 fizz_buzz(18)
