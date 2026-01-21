f3ebf8192d354bd25ebdab19a8e40236ac15781d
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-01-21 21:59:48 +0000

    Dictionary

---
+++ 06_Dictionary/B_dictionary/letter_map.py
@@ -0,0 +1,14 @@
+# Write a function `letter_map` that accepts:
+# - a string
+# - a dictionary
+# The function should return a new string where characters that appear as keys in the dictionary are replaced with their corresponding values.
+
+print(letter_map("symbolic", {"y":"i","o":"a","c":"k" }))
+# 'simbalik'
+
+print(letter_map("colossal", {"o":"x","s":"p" }))
+# 'cxlxppal'
+
+print(letter_map("miniscule", {"u":"t","i":"f","e":"q" }))
+# 'mfnfsctlq'
+
