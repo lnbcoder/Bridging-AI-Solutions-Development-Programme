29686af0d4551dbc5b216d406569a4741700fbb3
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-01-21 21:38:38 +0000

    Dictionary

---
+++ 06_Dictionary/B_dictionary/character_count.py
@@ -0,0 +1,21 @@
+# Write a function `character_count` that accepts a string as an argument.
+# The function should return a **dictionary** containing the count of each character in the string.
+
+def character_count(str):
+    str_list = len(str)
+    count = 0
+    
+    for i in range(str_list):
+        print(str[i])
+
+    #return str_list
+
+character_count("evening")
+# print(character_count("evening"))
+# { 'e': 2, 'v': 1, 'n': 2, 'i': 1, 'g': 1 }
+
+print(character_count("mississippi"))
+# { 'm': 1, 'i': 4, 's': 4, 'p': 2 }
+
+print(character_count("chili"))
+# { 'c': 1, 'h': 1, 'i': 2, 'l': 1 }
