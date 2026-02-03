49747b27d0faaa118dc0ac26bead876574f4bf23
Loveness Baloyi <lovenessntshuxi12@gmail.com>
2026-02-03 22:38:39 +0200

    Update

--- snippets.py
+++ snippets.py
@@ -1,10 +1,3 @@
-age = [23,25]
-
-def age_2026_2027():
-    age[0] = 24
-    age[1] = 24 + 2
-    print(age)
-
-print(age)
-age_2026_2027()
-print(age)
+unpack = "name"
+print(*unpack)
+#a,b,c,d = *unpack
