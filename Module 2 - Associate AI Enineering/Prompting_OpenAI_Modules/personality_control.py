7c0eedff14d2a4b38c95a000fd41b789d2d7fa80
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-02-19 23:44:09 +0200

    OpenAI API Exercise
    Implement personality control for a strict math teacher assistant.

---
+++ Module 2 - Associate AI Enineering/Prompting_OpenAI_Modules/personality_control.py
@@ -0,0 +1,31 @@
+# Personality Control
+# Create a script that:
+#  1.Uses a system message to make the assistant act like a strict math teacher.
+#  2.Uses a user message that asks:
+#   "Explain what 12 * 8 equals."
+#  3.Prints the result.
+# Goal: Observe how much the system message controls tone and style.
+
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("OPENAI_API_KEY not found in the .env")
+
+client = OpenAI(api_key=api_key)
+
+response = client.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages = [{
+        "role":"system","content":"act like a strict math teacher",
+        "role":"user","content":"act like a strict math teacher"
+    }],
+    max_completion_tokens = 200
+)
+
+print(response.choices[0].message.content)
