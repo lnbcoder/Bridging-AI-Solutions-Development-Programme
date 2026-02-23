d6c25776f6ff1c90f892903bacf63f29a8d95843
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-02-23 21:18:43 +0200

    Zero Shot - Eexercise 2

---
+++ Module 2 - Associate AI Enineering/Prompting_OpenAI_Modules/structureded_output.py
@@ -0,0 +1,15 @@
+'''
+# Exercise 2: Structured Output (JSON Extraction)
+
+# Prompt:
+#  Extract the following information from the text below:
+#   ->Name
+#   -> Email
+#   -> Company
+
+# Return the result in valid JSON format with the keys: "name", "email", and "company".
+
+# Text:
+#  “Hi, I’m Sarah Chen from BrightWave. You can reach me at sarah@brightwave.io.”
+
+'''
