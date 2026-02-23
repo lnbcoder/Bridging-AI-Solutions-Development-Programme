c79767e4e880d5e2fbb33f301f93f12fe6677c35
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-02-23 21:23:27 +0200

    One Shot - Exercise 3

---
+++ Module 2 - Associate AI Enineering/Prompting_OpenAI_Modules/tone_transformation.py
@@ -0,0 +1,13 @@
+# Exercise 3: Tone Transformation
+
+# Prompt:
+#   Rewrite the following formal text into a casual tone.
+
+# Formal:
+#   “We regret to inform you that your application has been denied.”
+
+# Casual:
+#   “Hey, just a heads up — we weren’t able to approve your application this time.”
+
+# Now rewrite this in a casual tone:
+#“Your request has been received and is currently under review.”
