227024f9338edeaab6fcd025d0f7536264a32d05
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-02-23 21:40:48 +0200

    Few Shot - Exercise 5

---
+++ Module 2 - Associate AI Enineering/Prompting_OpenAI_Modules/emotion_detection.py
@@ -0,0 +1,17 @@
+# Exercise 5: Emotion Detection
+
+# Prompt:
+
+# Classify the emotion of each sentence as Joy, Anger, Sadness, or Fear.
+
+# Sentence: “I just got promoted at work!”
+# Emotion: Joy
+
+# Sentence: “I can’t believe they lied to me like that.”
+# Emotion: Anger
+
+# Sentence: “I miss her more than I can explain.”
+# Emotion: Sadness
+
+#Sentence: “I don’t know what’s going to happen, and it’s keeping me up at night.”
+# Emotion:
