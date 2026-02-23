585a8271499fcb008ce1dd92461d2c398baea83d
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-02-23 21:42:41 +0200

    Few Shot - Exercise 6

---
+++ Module 2 - Associate AI Enineering/Prompting_OpenAI_Modules/logical_analogy_pattern.py
@@ -0,0 +1,8 @@
+# Exercise 6: Logical Analogy Pattern
+
+# Prompt:
+
+# Solve the analogy.
+# Tree is to Forest as Star is to Galaxy.
+# Hand is to Glove as Foot is to Sock.
+# Bird is to Sky as Fish is to ?
