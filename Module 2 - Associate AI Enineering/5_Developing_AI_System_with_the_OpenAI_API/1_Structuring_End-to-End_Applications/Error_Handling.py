10bc6fb8cadc63e0d49272bba238fe5c942fa30e
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-04-26 22:51:13 +0200

    Add error handling for OpenAI API requests

---
+++ Module 2 - Associate AI Enineering/5_Developing_AI_System_with_the_OpenAI_API/1_Structuring_End-to-End_Applications/Error_Handling.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+try:
+  response=client.chat.completions.create(
+  model="gpt-4.1-mini",
+  messages=[
+          {"role":"system","content":"You are a helpful assistant"},
+          {"role":"user","content":"Explain what an API is"}
+      ]
+  )
+
+print(response.choices[0].message.content)
+
+exceptExceptionase:
+  print("Something went wrong. Please try again later.")
+
+
