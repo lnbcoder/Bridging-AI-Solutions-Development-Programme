c62620962d9001f88fe3f719e4d926830b517d55
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-04-26 21:44:15 +0200

    Launch Gradio interface in browser

---
+++ Module 2 - Associate AI Enineering/UI/Gradio/2_Using_inbrowser.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+import gradio as gr
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+def greet(text):
+    print(f"Shout has been called with input {text}")
+    return text.upper()
+
+greet("example text")
+
+gr.Interface(fn=greet, inputs="textbox", outputs="textbox", flagging_mode="never").launch(inbrowser=True)
