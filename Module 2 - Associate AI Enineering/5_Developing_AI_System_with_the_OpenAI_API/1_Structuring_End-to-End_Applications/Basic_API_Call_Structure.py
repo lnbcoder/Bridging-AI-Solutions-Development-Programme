6ba125d701d88ef0888e98b68506be83138dd008
Loveness Baloyi <141639896+lnbcoder@users.noreply.github.com>
2026-04-26 22:38:08 +0200

    Add basic API call structure using OpenAI API

---
+++ Module 2 - Associate AI Enineering/5_Developing_AI_System_with_the_OpenAI_API/1_Structuring_End-to-End_Applications/Basic_API_Call_Structure.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+import gradio as gr
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+response=client.chat.completions.create(
+model="gpt-4.1-mini",
+messages=[
+        {"role":"system","content":"You are a helpful assistant"},
+        {"role":"user","content":"Explain what an API is"}
+    ]
+)
+
+print(response.choices[0].message.content)
